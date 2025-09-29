bd0ccbb274a9cccf618cf331c622a3cd955e8e9e
SonalAmarasekera <164004817+SonalAmarasekera@users.noreply.github.com>
2025-09-30 01:56:13 +0800

    Fixing latent caching script name

--- Gen_latents.sh
+++ Gen_latents.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-chmod +x cache_latents.py
+chmod +x cache_latents_working.py
 
 # Generate Latents
-python cache_latents.py --csv train.csv --out_dir ~/thesis1/latents/train
+python cache_latents_working.py --csv train.csv --out_dir /content/latents/train
 
-python cache_latents.py --csv dev.csv --out_dir ~/thesis1/latents/dev
+python cache_latents_working.py --csv dev.csv --out_dir content/latents/dev
 
-python cache_latents.py --csv test.csv --out_dir ~/thesis1/latents/test
+python cache_latents_working.py --csv test.csv --out_dir /content/latents/test
