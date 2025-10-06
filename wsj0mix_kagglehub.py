f4cd1e2d1ace75b5a5ebda7bcd7cb3915eb7d0d6
SonalAmarasekera <164004817+SonalAmarasekera@users.noreply.github.com>
2025-10-07 05:11:49 +0800

    Add files via upload

---
+++ wsj0mix_kagglehub.py
@@ -0,0 +1,6 @@
+import kagglehub
+
+# Download latest version
+path = kagglehub.dataset_download("sonishmaharjan555/wsj0-2mix")
+
+print("Path to dataset files:", path)
---
+++ wsj0mix_quickstart.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Cloning the needed repos
+git clone https://github.com/mpariente/pywsj0-mix.git
+
+# Install kagglehub
+pip install kagglehub
+
+# Get the root files from Kagglehub
+python wsj0mix_kagglehub.py
+
+# Move the data files to accessible space
+mv /root/.cache/kagglehub/datasets/sonishmaharjan555/wsj0-2mix/versions/2 .
+
+cd pywsj0-mix/
+
+echo "Starting data creation..."
+# Generate data for 2 speakers at 8k
+python generate_wsjmix.py -p /content/2 -o /content/ -n 2 -sr 8000
