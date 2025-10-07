b188920166da7110a45b2c749bf4b5619ee545fa
SonalAmarasekera <164004817+SonalAmarasekera@users.noreply.github.com>
2025-10-07 21:53:51 +0800

    Update gen_latents.sh

--- WSJ0/gen_latents.sh
+++ WSJ0/gen_latents.sh
@@ -3,8 +3,8 @@
 chmod +x cache_latents_rwkv_v7_fixed_pb.py
 
 # Generate Latents
-python cache_latents_rwkv_v7_fixed_pb.py --csv train_new.csv --out_dir /content/latents/train
+python cache_latents_rwkv_v7_fixed_pb.py --csv train_min.csv --out_dir /content/latents/train
 
-python cache_latents_rwkv_v7_fixed_pb.py --csv dev_new.csv --out_dir /content/latents/dev
+python cache_latents_rwkv_v7_fixed_pb.py --csv dev_min.csv --out_dir /content/latents/dev
 
-python cache_latents_rwkv_v7_fixed_pb.py --csv test_new.csv --out_dir /content/latents/test
+python cache_latents_rwkv_v7_fixed_pb.py --csv test_min.csv --out_dir /content/latents/test
