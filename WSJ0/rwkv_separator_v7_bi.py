c18a917be9e8dbe36be1b8616ee189597e6955fb
SonalAmarasekera <164004817+SonalAmarasekera@users.noreply.github.com>
2025-10-11 01:43:07 +0800

    Update rwkv_separator_v7_bi.py

--- WSJ0/rwkv_separator_v7_bi.py
+++ WSJ0/rwkv_separator_v7_bi.py
@@ -8,7 +8,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 # Import official RWKV-v7 components from the author's file
-from rwkv_orig_model import RWKV_Tmix_x070, RWKV_CMix_x070
+from RWKV-LM.RWKV-v5.src.model import RWKV_Tmix_x070, RWKV_CMix_x070
 
 # ----------------------- Configs -----------------------
 
