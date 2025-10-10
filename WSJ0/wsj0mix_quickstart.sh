82d2bbbbcdf906c116b751bcd112ff6986f92fe4
SonalAmarasekera <164004817+SonalAmarasekera@users.noreply.github.com>
2025-10-11 02:52:45 +0800

    Update wsj0mix_quickstart.sh

--- WSJ0/wsj0mix_quickstart.sh
+++ WSJ0/wsj0mix_quickstart.sh
@@ -25,6 +25,8 @@ echo "Starting data creation..."
 # Generate data for 2 speakers at 8k
 python generate_wsjmix.py -p /content/2 -o /content/ -n 2 -sr 8000
 
+cd ../
+
 # Mapping the CSVs
 python make_csv.py --root /content/2speakers/wav8k/min/tr --out train_min.csv
 python make_csv.py --root /content/2speakers/wav8k/min/cv --out dev_min.csv
