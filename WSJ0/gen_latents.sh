0eb0624bf36a5533d5579c94a26adea5b4dda761
SonalAmarasekera <164004817+SonalAmarasekera@users.noreply.github.com>
2025-10-23 02:58:29 +0800

    Update gen_latents.sh

--- WSJ0/gen_latents.sh
+++ WSJ0/gen_latents.sh
@@ -3,8 +3,8 @@
 chmod +x cache_latents_working.py
 
 # Generate Latents
-#python cache_latents_working.py --csv train_min.csv --out_dir /content/latents/min/train --model_type 16khz --device cuda
+#python cache_latents_working.py --csv train_min.csv --out_dir /workspace/latents/min/train --model_type 16khz --device cuda
 
-python cache_latents_working.py --csv dev_min.csv --out_dir /content/latents/min/dev --model_type 16khz --device cuda
+python cache_latents_working.py --csv dev_min.csv --out_dir /workspace/latents/min/dev --model_type 16khz --device cuda
 
-python cache_latents_working.py --csv test_min.csv --out_dir /content/latents/min/test --model_type 16khz --device cuda
+python cache_latents_working.py --csv test_min.csv --out_dir /workspace/latents/min/test --model_type 16khz --device cuda
