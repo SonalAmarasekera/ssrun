9cc5134b431ff7abcf5a673f28d5eb113048575a
SonalAmarasekera <164004817+SonalAmarasekera@users.noreply.github.com>
2025-11-14 10:24:24 +0800

    Update gen_latents_fixed.sh

--- fixed/gen_latents_fixed.sh
+++ fixed/gen_latents_fixed.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-chmod +x cache_latents_fixed.py
-
 # Generate Latents
-python cache_latents_fixed.py --csv train_min.csv --out_dir /workspace/latents/min/train --model_type 16khz --device cuda
+python cache_latents.py --csv train_min.csv --out_dir /workspace/latents/min/train --model_type 16khz --device cuda
 
-python cache_latents_fixed.py --csv dev_min.csv --out_dir /workspace/latents/min/dev --model_type 16khz --device cuda
+python cache_latents.py --csv dev_min.csv --out_dir /workspace/latents/min/dev --model_type 16khz --device cuda
 
-#python cache_latents_fixed.py --csv test_min.csv --out_dir /workspace/latents/min/test --model_type 16khz --device cuda
+python cache_latents.py --csv test_min.csv --out_dir /workspace/latents/min/test --model_type 16khz --device cuda
